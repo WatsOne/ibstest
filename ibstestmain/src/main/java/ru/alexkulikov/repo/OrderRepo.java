5fc0074b97b3ee13775cd57a83d8cd0b0603d7d9
WatsOne <thewato@gmail.com>
2018-02-06 01:04:52 +0500

    data

--- ibstestmain/src/main/java/ru/alexkulikov/repo/OrderRepo.java
+++ ibstestmain/src/main/java/ru/alexkulikov/repo/OrderRepo.java
@@ -1,18 +1,24 @@
 package ru.alexkulikov.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.alexkulikov.entity.Order;
 import ru.alexkulikov.entity.OrderState;
-import ru.alexkulikov.entity.Person;
 
 import javax.annotation.PostConstruct;
 
 @Repository
 public class OrderRepo extends MemoryRepo<Order> {
 
+    @Autowired
+    private PersonRepo personRepo;
+
     @PostConstruct
     public void init() {
-        entities.add(new Order(123, "order1", new Person("as", "asd"), new Person("as", "asd"), OrderState.CREATED));
-        entities.add(new Order(126, "order2", new Person("ass", "aasd"), new Person("a4s", "a5sd"), OrderState.FINISHED));
+        entities.add(new Order(123, "Заявка на йогурт", personRepo.getRandom(), personRepo.getRandom(), OrderState.CREATED));
+        entities.add(new Order(126, "Заявка на сметану", personRepo.getRandom(), personRepo.getRandom(), OrderState.FINISHED));
+        entities.add(new Order(129, "Заявка на молоко", personRepo.getRandom(), personRepo.getRandom(), OrderState.PROCESSED));
+        entities.add(new Order(132, "Заявка на сыр", personRepo.getRandom(), personRepo.getRandom(), OrderState.CREATED));
+        entities.add(new Order(135, "Заявка на творог", personRepo.getRandom(), personRepo.getRandom(), OrderState.FINISHED));
     }
 }
--- ibstestmain/src/main/java/ru/alexkulikov/repo/PersonRepo.java
+++ ibstestmain/src/main/java/ru/alexkulikov/repo/PersonRepo.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Repository;
 import ru.alexkulikov.entity.Person;
 
 import javax.annotation.PostConstruct;
+import java.util.Random;
 
 @Repository
 public class PersonRepo extends MemoryRepo<Person> {
 
+    private Random rnd;
+
     @PostConstruct
     public void init() {
-        entities.add(new Person("aaa", "bbb"));
-        entities.add(new Person("ccc", "ddd"));
+        rnd = new Random();
+
+        entities.add(new Person("Александр", "Пушкин"));
+        entities.add(new Person("Лев", "Толстой"));
+        entities.add(new Person("Фёдор", "Достоевский"));
+        entities.add(new Person("Николай", "Гоголь"));
+        entities.add(new Person("Илон", "Маск"));
+        entities.add(new Person("Владимир", "Ленин"));
+        entities.add(new Person("Иван", "Тургенев"));
+        entities.add(new Person("Иосиф", "Сталин"));
+    }
+
+    public Person getRandom() {
+        return entities.get(rnd.nextInt(entities.size()));
     }
 }
