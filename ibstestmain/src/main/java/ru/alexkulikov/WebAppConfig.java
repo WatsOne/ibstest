3a78580a888e0c3d638b67e603de4177287d5995
WatsOne <thewato@gmail.com>
2018-02-05 22:53:09 +0500

    server structure

--- ibstestmain/src/main/java/ru/alexkulikov/WebAppConfig.java
+++ ibstestmain/src/main/java/ru/alexkulikov/WebAppConfig.java
@@ -1,28 +1,12 @@
 package ru.alexkulikov;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan ("ru.alexkulikov")
-public class WebAppConfig extends WebMvcConfigurerAdapter {
+public class WebAppConfig {
 
-    @Bean
-    public InternalResourceViewResolver resolver() {
-	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-	resolver.setPrefix("/WEB-INF/pages/");
-	resolver.setSuffix(".html");
-	return resolver;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-    }
 }
